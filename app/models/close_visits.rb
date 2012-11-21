e7d1769ac84376665c51a9d844201e5103951d80
phundi <kennethkapundi1@gmail.com>
2012-11-21 16:44:13 +0200

    integrated apgar windows into one window; updated concepts

--- app/models/close_visits.rb
+++ app/models/close_visits.rb
@@ -1,6 +1,6 @@
 class CloseVisits < ActiveRecord::Base  
   #to be run from script/console with Visit.close_visits
-  #TODO assign a dummy table name here
+  # assign any table to make the model work, run from console
   set_table_name :visit
   def self.close_visits
     
@@ -13,13 +13,14 @@ class CloseVisits < ActiveRecord::Base
       failed_attempts = close_patient_visits(patient)
     
       failed_visits[patient] = failed_attempts if failed_attempts > 0
-
+      
+      puts "DONE"
       # return a hash summary of failed records   
 
     end
     return failed_visits
   end 
-  #close all visits for a particular patient using optimal enddate check
+  #close all visits for a particular patient using optimal end date check
   def self.close_patient_visits(patient)
     failed_records = 0
     patient_visits = Visit.find(:all, :conditions => ["patient_id = ?", patient])
@@ -32,9 +33,10 @@ class CloseVisits < ActiveRecord::Base
 
           visit.update_attributes(:end_date => closing_datetime_estimate) if closing_datetime_estimate
           visit.update_attributes(:end_date => last_encounter.date_created) if visit.end_date.nil?
-          visit.update_attributes(:end_date => Time.now.strftime("%Y-%m-%d %H:%M:%S")) if visit.end_date.nil?
-
+          #visit.update_attributes(:end_date => Time.now.strftime("%Y-%m-%d %H:%M:%S")) if visit.end_date.nil?
+          puts "updating visit for patient #{visit.patient_id}"
           failed_records +=1 if visit.end_date.nil?
+          puts "#{failed_records}" if visit.end_date.nil?
         end
 			
       end
