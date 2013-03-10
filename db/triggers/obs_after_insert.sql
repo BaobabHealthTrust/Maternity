9833dbfec1bed726f2f05139c830dffe3b857c41
Kenneth <vine@vine.(none)>
2013-03-10 21:15:31 +0200

    updated triggers

--- db/triggers/obs_after_insert.sql
+++ db/triggers/obs_after_insert.sql
@@ -40,12 +40,7 @@ BEGIN
   		INSERT INTO patient_report (patient_id, outcome, outcome_date, obs_datetime, obs_id) VALUES(new.person_id, @mode, new.obs_datetime, new.obs_datetime, new.obs_id);
 	END IF;
 	
-	IF new.concept_id = (SELECT concept_id FROM concept_name WHERE name = "ADMISSION SECTION" LIMIT 1) THEN
-		SET @ward = (SELECT name FROM location WHERE location_id = new.location_id);	
-
-  		INSERT INTO patient_report (patient_id, admission_ward, admission_date, obs_datetime, obs_id) VALUES(new.person_id, @ward, new.value_datetime, new.value_datetime, new.obs_id);
-	END IF;
-	
+		
 	IF new.concept_id = (SELECT concept_id FROM concept_name WHERE name = "DIAGNOSIS" LIMIT 1) THEN
 		SET @diagnosis = (SELECT name FROM concept_name WHERE concept_name_id = new.value_coded_name_id);	
 
@@ -145,11 +140,17 @@ IF new.concept_id = (SELECT concept_id FROM concept_name WHERE name = "BILATERAL
 
 
 	IF new.concept_id = (SELECT concept_id FROM concept_name WHERE name = "ADMISSION SECTION" LIMIT 1) AND COALESCE(new.value_modifier, '') != '' THEN
-		SET @ward = (SELECT name FROM location WHERE location_id = new.value_modifier LIMIT 1);	
+			SET @ward = (SELECT name FROM location WHERE location_id = new.value_modifier LIMIT 1);		
 
   		INSERT INTO patient_report (patient_id, source_ward, destination_ward, internal_transfer_date, obs_datetime, obs_id) VALUES(new.person_id, @ward, new.value_text, new.obs_datetime, new.obs_datetime, new.obs_id);
 
 		UPDATE patient_report SET last_ward_where_seen = new.value_text, last_ward_where_seen_date = new.obs_datetime WHERE COALESCE(delivery_mode,'') != '' AND patient_id = new.person_id AND delivery_date >= DATE_ADD(new.obs_datetime, INTERVAL -7 DAY) AND delivery_date <= DATE_ADD(new.obs_datetime, INTERVAL 7 DAY);
+	END IF;
+
+	IF new.concept_id = (SELECT concept_id FROM concept_name WHERE name = "ADMISSION SECTION" LIMIT 1) THEN
+		SET @ward = (SELECT name FROM location WHERE location_id = new.location_id LIMIT 1);	
+
+  		INSERT INTO patient_report (patient_id, admission_ward, admission_date, obs_datetime, obs_id) VALUES(new.person_id, @ward, new.value_datetime, new.value_datetime, new.obs_id);
 	END IF;
 	
 	IF new.concept_id = (SELECT concept_id FROM concept_name WHERE name = "IS PATIENT REFERRED?" LIMIT 1) AND new.value_coded IN (SELECT concept_id FROM concept_name WHERE name = "Yes") THEN
