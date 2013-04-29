c6048f0298da3f59ab528086da420d39946ae761
Kenneth <vine@vine.(none)>
2013-04-29 16:41:13 +0200

    checked filters for searched locations

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -52,7 +52,7 @@ class PeopleController < ApplicationController
   # List traditional authority containing the string given in params[:value]
   def traditional_authority
     district_id = District.find_by_name("#{params[:filter_value]}").id
-    traditional_authority_conditions = ["name LIKE (?) AND district_id = ?", "#{params[:search_string]}%", district_id]
+    traditional_authority_conditions = ["name LIKE (?) AND district_id = ?", "%#{params[:search_string]}%", district_id]
 
     traditional_authorities = TraditionalAuthority.find(:all,:conditions => traditional_authority_conditions, :order => 'name')
     traditional_authorities = traditional_authorities.map do |t_a|
@@ -63,7 +63,7 @@ class PeopleController < ApplicationController
 
   # Regions containing the string given in params[:value]
   def region
-    region_conditions = ["name LIKE (?)", "#{params[:value]}%"]
+    region_conditions = ["name LIKE (?)", "%#{params[:value]}%"]
 
     regions = Region.find(:all,:conditions => region_conditions, :order => 'name')
     regions = regions.map do |r|
@@ -75,7 +75,7 @@ class PeopleController < ApplicationController
   # Districts containing the string given in params[:value]
   def district
     region_id = Region.find_by_name("#{params[:filter_value]}").id
-    region_conditions = ["name LIKE (?) AND region_id = ? ", "#{params[:search_string]}%", region_id]
+    region_conditions = ["name LIKE (?) AND region_id = ? ", "%#{params[:search_string]}%", region_id]
 
     districts = District.find(:all,:conditions => region_conditions, :order => 'name')
     districts = districts.map do |d|
@@ -87,7 +87,7 @@ class PeopleController < ApplicationController
   # Villages containing the string given in params[:value]
   def village
     traditional_authority_id = TraditionalAuthority.find_by_name("#{params[:filter_value]}").id
-    village_conditions = ["name LIKE (?) AND traditional_authority_id = ?", "#{params[:search_string]}%", traditional_authority_id]
+    village_conditions = ["name LIKE (?) AND traditional_authority_id = ?", "%#{params[:search_string]}%", traditional_authority_id]
 
     villages = Village.find(:all,:conditions => village_conditions, :order => 'name')
     villages = villages.map do |v|
