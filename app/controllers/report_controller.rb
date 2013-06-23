efadf26a0f673683074f24685315f6cb68669cdd
Kenneth <vine@vine.(none)>
2013-06-23 22:58:21 +0200

    linked mother addresses to baby, extended demographics fields for baby, added communication interface for vitals reports

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -12,20 +12,20 @@ class ReportController < ApplicationController
     end
     
     @diagnoses = ConceptName.find(:all,
-                                  :joins =>
-                                        "INNER JOIN obs ON
+      :joins =>
+        "INNER JOIN obs ON
                                          concept_name.concept_id = obs.value_coded",
-                                  :conditions => ["date_format(obs_datetime, '%Y-%m-%d') >= ? AND date_format(obs_datetime, '%Y-%m-%d') <= ?",
-                                            @start_date, @end_date],
-                                  :group =>   "name",
-                                  :select => "concept_name.concept_id,concept_name.name,obs.value_coded,obs.obs_datetime,obs.voided")
+      :conditions => ["date_format(obs_datetime, '%Y-%m-%d') >= ? AND date_format(obs_datetime, '%Y-%m-%d') <= ?",
+        @start_date, @end_date],
+      :group =>   "name",
+      :select => "concept_name.concept_id,concept_name.name,obs.value_coded,obs.obs_datetime,obs.voided")
     @patient = Person.find(:all,
-                           :joins => 
-                                "INNER JOIN obs ON 
+      :joins =>
+        "INNER JOIN obs ON
                                  person.person_id = obs.person_id",
-                           :conditions => ["date_format(obs_datetime, '%Y-%m-%d') >= ? AND date_format(obs_datetime, '%Y-%m-%d') <= ?",
-                                            @start_date, @end_date],
-                           :select => "person.voided,obs.value_coded,obs.obs_datetime,obs.voided ")
+      :conditions => ["date_format(obs_datetime, '%Y-%m-%d') >= ? AND date_format(obs_datetime, '%Y-%m-%d') <= ?",
+        @start_date, @end_date],
+      :select => "person.voided,obs.value_coded,obs.obs_datetime,obs.voided ")
   
     @times = []                         
     @data_hash = Hash.new
@@ -73,91 +73,91 @@ class ReportController < ApplicationController
         @data_hash.each{|time,data|
           if t.to_date == time.to_date 
             data.each{|k,v|
-            if k == d.name
-              sum = sum + v 
-            end
-          }
+              if k == d.name
+                sum = sum + v
+              end
+            }
           end
+        }
+
+
       }
-
-
-    }
-    sort_hash[d.name] = sum
+      sort_hash[d.name] = sum
 
     }
 
-  sort_hash = sort_hash.sort{|a,b| -1*( a[1]<=>b[1])}
-   sort_hash.each{|x| @sort_array << x[0]}
+    sort_hash = sort_hash.sort{|a,b| -1*( a[1]<=>b[1])}
+    sort_hash.each{|x| @sort_array << x[0]}
 
-  # make_and_send_pdf('/report/weekly_report', 'weekly_report.pdf')
+    # make_and_send_pdf('/report/weekly_report', 'weekly_report.pdf')
 
   end
 
   def disaggregated_diagnosis
 
-  @start_date = Date.new(params[:start_year].to_i,params[:start_month].to_i,params[:start_day].to_i) rescue nil
-  @end_date = Date.new(params[:end_year].to_i,params[:end_month].to_i,params[:end_day].to_i) rescue nil
-   if (@start_date > @end_date) || (@start_date > Date.today)
+    @start_date = Date.new(params[:start_year].to_i,params[:start_month].to_i,params[:start_day].to_i) rescue nil
+    @end_date = Date.new(params[:end_year].to_i,params[:end_month].to_i,params[:end_day].to_i) rescue nil
+    if (@start_date > @end_date) || (@start_date > Date.today)
       flash[:notice] = 'Start date is greater than end date or Start date is greater than today'
       redirect_to :action => 'select'
       return
     end
 
-  #getting an array of all diagnoses recorded within the chosen period - to avoid including existent but non recorded diagnoses
-  diagnoses = ConceptName.find(:all,
-                                  :joins =>
-                                        "INNER JOIN obs ON
+    #getting an array of all diagnoses recorded within the chosen period - to avoid including existent but non recorded diagnoses
+    diagnoses = ConceptName.find(:all,
+      :joins =>
+        "INNER JOIN obs ON
                                          concept_name.concept_id = obs.value_coded",
-                                  :conditions => ["date_format(obs_datetime, '%Y-%m-%d') >= ? AND date_format(obs_datetime, '%Y-%m-%d') <= ?",
-                                            @start_date, @end_date],
-                                  :group =>   "name",
-                                  :select => "concept_name.concept_id,concept_name.name,obs.value_coded,obs.obs_datetime,obs.voided")
-  #getting list of all patients who were diagnosed within the set period-to avoid getting all patients                          
-  @patient = Person.find(:all,
-                           :joins => 
-                                "INNER JOIN obs ON 
+      :conditions => ["date_format(obs_datetime, '%Y-%m-%d') >= ? AND date_format(obs_datetime, '%Y-%m-%d') <= ?",
+        @start_date, @end_date],
+      :group =>   "name",
+      :select => "concept_name.concept_id,concept_name.name,obs.value_coded,obs.obs_datetime,obs.voided")
+    #getting list of all patients who were diagnosed within the set period-to avoid getting all patients
+    @patient = Person.find(:all,
+      :joins =>
+        "INNER JOIN obs ON
                                  person.person_id = obs.person_id",
-                           :conditions => ["date_format(obs_datetime, '%Y-%m-%d') >= ? AND date_format(obs_datetime, '%Y-%m-%d') <= ?",
-                                            @start_date, @end_date],
-                           :select => "person.gender,person.birthdate,person.birthdate_estimated,person.date_created,
+      :conditions => ["date_format(obs_datetime, '%Y-%m-%d') >= ? AND date_format(obs_datetime, '%Y-%m-%d') <= ?",
+        @start_date, @end_date],
+      :select => "person.gender,person.birthdate,person.birthdate_estimated,person.date_created,
                                       person.voided,obs.value_coded,obs.obs_datetime,obs.voided ")
   
-  sort_hash = Hash.new
+    sort_hash = Hash.new
 
-  #sorting the diagnoses using frequency with the highest first
-  diagnoses.each{|diagnosis|
-    count = 0
-    @patient.each{|patient|
-      if patient.value_coded == diagnosis.value_coded
-        count += 1
-      end
-    }
-    sort_hash[diagnosis.name] = count
+    #sorting the diagnoses using frequency with the highest first
+    diagnoses.each{|diagnosis|
+      count = 0
+      @patient.each{|patient|
+        if patient.value_coded == diagnosis.value_coded
+          count += 1
+        end
+      }
+      sort_hash[diagnosis.name] = count
   
-  }
-  #A sorted array of diagnoses to be sent to be sent to form
-  @diagnoses = Array.new
+    }
+    #A sorted array of diagnoses to be sent to be sent to form
+    @diagnoses = Array.new
 
-   sort_hash = sort_hash.sort{|a,b| -1*( a[1]<=>b[1])}
-   diagnosis_names = []
-   sort_hash.each{|x| diagnosis_names << x[0]}
-   diagnosis_names.each{|d|
-     diagnoses.each{|diag|
-       @diagnoses << diag if d == diag.name     
-     }
-   }
+    sort_hash = sort_hash.sort{|a,b| -1*( a[1]<=>b[1])}
+    diagnosis_names = []
+    sort_hash.each{|x| diagnosis_names << x[0]}
+    diagnosis_names.each{|d|
+      diagnoses.each{|diag|
+        @diagnoses << diag if d == diag.name
+      }
+    }
    
 
   end
 
   def referral
-     @start_date = Date.new(params[:start_year].to_i,params[:start_month].to_i,params[:start_day].to_i) rescue nil
+    @start_date = Date.new(params[:start_year].to_i,params[:start_month].to_i,params[:start_day].to_i) rescue nil
     @end_date = Date.new(params[:end_year].to_i,params[:end_month].to_i,params[:end_day].to_i) rescue nil
-      if (@start_date > @end_date) || (@start_date > Date.today)
-        flash[:notice] = 'Start date is greater than end date or Start date is greater than today'
-        redirect_to :action => 'select'
-        return
-      end
+    if (@start_date > @end_date) || (@start_date > Date.today)
+      flash[:notice] = 'Start date is greater than end date or Start date is greater than today'
+      redirect_to :action => 'select'
+      return
+    end
 
     @referrals = Observation.find(:all, :conditions => ["concept_id = ? AND date_format(obs_datetime, '%Y-%m-%d') >= ? AND 
                                   date_format(obs_datetime, '%Y-%m-%d') <= ?", 2227, @start_date, @end_date])
@@ -195,5 +195,17 @@ class ReportController < ApplicationController
     make_and_send_pdf('/report/weekly_report', 'weekly_report.pdf')
   end
 
+  def issue_baby_ids
+
+    national_ids = []
+
+    BirthReport.find(:all, :conditions => ["acknowledged IS NOT NULL AND DATE(date_created) >= ? AND DATE(date_created) <= ?",
+        params[:start_date].to_date, params[:end_date].to_date]).each{|br|
+      national_ids << Patient.find(br.person_id).national_id rescue nil
+    }
+    
+    ids = {"ids" => national_ids}
+    render :text => ids.to_json
+  end
 
 end
